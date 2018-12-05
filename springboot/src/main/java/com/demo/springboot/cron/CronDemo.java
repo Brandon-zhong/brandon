bedeaa64b0dd9466e5203866b73f6b450e557fc3
brandon-zhong <brandon-zhong8859@foxmail.com>
2018-12-05 12:35:52 +0800

    update

---
+++ springboot/src/main/java/com/demo/springboot/cron/CronDemo.java
@@ -0,0 +1,19 @@
+package com.demo.springboot.cron;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author: brandon
+ * @Date: 2018-11-30
+ * @Description:
+ */
+@Component
+public class CronDemo {
+
+    @Scheduled(cron = "0 0/2 * * * ? *")
+    public void doTask(){
+        System.out.println("执行了一次计划任务");
+    }
+
+}
