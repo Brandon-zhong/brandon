f2849e9eb35cc54e803029c8c87872d5e7581a9b
brandon-zhong <brandon-zhong8859@foxmail.com>
2019-06-05 12:32:25 +0800

    更新

---
+++ demo/src/main/java/com/demo/tree/TreeOrder.java
@@ -0,0 +1,150 @@
+package com.demo.tree;
+
+/**
+ * @author brandon
+ * create on 2019-05-13
+ * desc: tree 的前、中、后序遍历
+ */
+public class TreeOrder<E> {
+
+    static final class Node<E> {
+
+        final E data;
+
+        boolean read = false;
+
+        Node<E> left;
+
+        Node<E> right;
+
+        Node<E> parent;
+
+        Node(E data) {
+            this.data = data;
+        }
+    }
+
+    private Node<String> getTree() {
+
+        Node<String> root = new Node<>("A");
+        Node<String> BNode = new Node<>("B");
+        Node<String> CNode = new Node<>("C");
+        Node<String> DNode = new Node<>("D");
+        Node<String> ENode = new Node<>("E");
+        Node<String> FNode = new Node<>("F");
+        Node<String> GNode = new Node<>("G");
+        Node<String> HNode = new Node<>("H");
+        Node<String> INode = new Node<>("I");
+        Node<String> JNode = new Node<>("J");
+
+        root.parent = null;
+
+        root.left = BNode;
+        BNode.parent = root;
+
+        BNode.left = DNode;
+        DNode.parent = BNode;
+
+        BNode.right = ENode;
+        ENode.parent = BNode;
+
+        DNode.left = HNode;
+        HNode.parent = DNode;
+
+        DNode.right = INode;
+        INode.parent = DNode;
+
+        ENode.left = JNode;
+        JNode.parent = ENode;
+
+        root.right = CNode;
+        CNode.parent = root;
+
+        CNode.left = FNode;
+        FNode.parent = CNode;
+
+        CNode.right = GNode;
+        GNode.parent = CNode;
+        return root;
+    }
+
+    /**
+     * 前序遍历
+     * 依次根节点，左节点，右节点
+     *
+     * @param root 根节点
+     */
+    public void preOrder(Node<E> root) {
+
+        if (root == null) {
+            return;
+        }
+        //打印根节点
+        System.out.print(root.data + " ");
+
+        //打印左节点
+        preOrder(root.left);
+
+        //打印右节点
+        preOrder(root.right);
+    }
+
+    /**
+     * 中序遍历
+     * 依次打印左节点，根节点，右节点
+     *
+     * @param root 根节点
+     */
+    public void inOrder(Node<E> root) {
+        if (root == null) {
+            return;
+        }
+        //打印左节点
+        inOrder(root.left);
+        //打印根节点
+        System.out.print(root.data + " ");
+
+        inOrder(root.right);
+    }
+
+    /**
+     * 后序遍历
+     * 依次打印左节点，右节点，根节点
+     *
+     * @param root 根节点
+     */
+    public void postOrder(Node<E> root) {
+        if (root == null) {
+            return;
+        }
+        //打印左节点
+        postOrder(root.left);
+        //打印右节点
+        postOrder(root.right);
+        //打印根节点
+        System.out.print(root.data + " ");
+    }
+
+    public void preOrderFor(Node<E> root) {
+        if (root == null) {
+            return;
+        }
+        Node<E> node = root;
+        for (; node != null; ) {
+            if (!node.read) {
+                node.read = true;
+                System.out.print(node.data + " ");
+            }
+            if (node.left != null && !node.left.read) {
+                node = node.left;
+                continue;
+            }
+            if (node.right != null && !node.right.read) {
+                node = node.right;
+                continue;
+            }
+            node = node.parent;
+        }
+    }
+
+}
---
+++ demo/src/main/java/com/demo/tree/TreeSearch.java
@@ -0,0 +1,78 @@
+package com.demo.tree;
+
+import java.util.HashMap;
+
+/**
+ * @author brandon
+ * create on 2019-05-15
+ * desc:
+ */
+public class TreeSearch extends TreeOrder<Integer> {
+
+
+    private Node<Integer> getSearchTree() {
+
+        int[] datas = new int[]{33, 16, 50, 13, 18, 34, 58, 15, 17, 25, 51, 66, 19, 27, 55};
+        Node<Integer> node = null;
+        for (int i : datas) {
+            node = add(node, i);
+        }
+        return node;
+    }
+
+    private Node<Integer> add(Node<Integer> root, int data) {
+
+        if (null == root) {
+            return new Node<>(data);
+        }
+        Node<Integer> node = root;
+        while (true) {
+            if (node.data > data) {
+                if (node.left == null) {
+                    node.left = new Node<>(data);
+                    return root;
+                }
+                node = node.left;
+            } else if (node.data < data) {
+                if (node.right == null) {
+                    node.right = new Node<>(data);
+                    return root;
+                }
+                node = node.right;
+            } else {
+                return root;
+            }
+        }
+    }
+
+    private Node<Integer> get(Node<Integer> root, int data) {
+
+        if (null == root) {
+            return null;
+        }
+        Node<Integer> node = root;
+        while (node != null) {
+            if (node.data == data) {
+                return node;
+            }
+            if (node.data > data) {
+                node = node.left;
+            } else if (node.data < data) {
+                node = node.right;
+            }
+        }
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+
+        TreeSearch treeSearch = new TreeSearch();
+        Node<Integer> tree = treeSearch.getSearchTree();
+        treeSearch.preOrder(tree);
+        Node<Integer> node = treeSearch.get(tree, 19);
+        System.out.println("\ndata --> " + (node == null ? 0 : node.data));
+
+    }
+
+}
