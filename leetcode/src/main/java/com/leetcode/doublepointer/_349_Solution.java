61a8e4843f86a0cae83f31f78ce580c2acf71bc8
brandon-zhong <brandon-zhong8859@foxmail.com>
2019-06-13 20:03:27 +0800

    更新

---
+++ leetcode/src/main/java/com/leetcode/doublepointer/_349_Solution.java
@@ -0,0 +1,44 @@
+package com.leetcode.doublepointer;
+
+/**
+ * @author brandon
+ * create on 2019-06-13
+ * desc: 两个数组的交集
+ * 给定两个数组，编写一个函数来计算它们的交集。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出: [2]
+ * 示例 2:
+ * <p>
+ * 输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出: [9,4]
+ * 说明:
+ * <p>
+ * 输出结果中的每个元素一定是唯一的。
+ * 我们可以不考虑输出结果的顺序。
+ * <p>
+ */
+public class _349_Solution {
+
+
+    public int[] intersection_1(int[] nums1, int[] nums2) {
+
+
+
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+
+
+
+
+
