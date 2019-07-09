0f6da9c7c75da8543226551bea345ebdc84e7824
brandon-zhong <brandon-zhong8859@foxmail.com>
2019-07-09 16:36:12 +0800

    更新

---
+++ demo/src/main/java/com/demo/concurrent/App.java
@@ -0,0 +1,19 @@
+package com.demo.concurrent;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author brandon
+ * create on 2019-07-01
+ * desc:
+ */
+public class App {
+
+    public static void main(String[] args) {
+
+        Lock lock = new ReentrantLock();
+
+    }
+
+}
