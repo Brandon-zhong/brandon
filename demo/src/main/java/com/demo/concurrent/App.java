51d19d17c030213dff4b119b2a6b7ce3f837b37c
zhongshengming <brandon-zhong8859@foxmail.com>
2019-08-04 10:37:24 +0800

    update

--- demo/src/main/java/com/demo/concurrent/App.java
+++ demo/src/main/java/com/demo/concurrent/App.java
@@ -1,5 +1,8 @@
 package com.demo.concurrent;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -9,11 +12,54 @@ import java.util.concurrent.locks.ReentrantLock;
  * desc:
  */
 public class App {
-
     public static void main(String[] args) {
-
-        Lock lock = new ReentrantLock();
-
+        new Thread(new TimeWaiting(), "TimeWaitingThread").start();
+        new Thread(new Waiting(), "WaitingThread").start();
+        new Thread(new Blocked(), "BlockedThread-1").start();
+        new Thread(new Blocked(), "BlockedThread-2").start();
     }
 
+    static class TimeWaiting implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                SleepUtils.second(100);
+            }
+        }
+    }
+
+    static class Waiting implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                synchronized (Waiting.class) {
+                    try {
+                        Waiting.class.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    static class Blocked implements Runnable {
+        @Override
+        public void run() {
+            synchronized (Blocked.class) {
+                while (true) {
+                    SleepUtils.second(100);
+                }
+            }
+        }
+    }
+}
+
+class SleepUtils {
+    public static final void second(long seconds) {
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {
+        }
+    }
 }
