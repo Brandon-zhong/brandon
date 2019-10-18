c88dcd4cf733718f68661aed315b2155eb6074d8
brandon <brandon-zhong8859@foxmail.com>
2019-10-18 22:02:05 +0800

    add leetcode

--- algorithm/src/main/java/com/leetcode/doublepointer/_234_Solution.java
+++ algorithm/src/main/java/com/leetcode/doublepointer/_234_Solution.java
@@ -37,8 +37,56 @@ public class _234_Solution {
         }
     }
 
-    public boolean isPalindrome(ListNode head) {
-       return false;
+    public boolean isPalindrome_1(ListNode head) {
+        ListNode first = head, secend = head;
+        //先确定链表的中点
+        Stack<Integer> stack = new Stack<>();
+        while (secend != null && secend.next != null) {
+            stack.push(first.val);
+            first = first.next;
+            secend = secend.next.next;
+        }
+        //长度为奇数的话，secend不为空，中点需要向前移动一位
+        if (secend != null) {
+            first = first.next;
+        }
+        //开始判断回文
+        while (first != null) {
+            if (stack.pop() != first.val) {
+                return false;
+            }
+            first = first.next;
+        }
+        return true;
+    }
+
+    public boolean isPalindrome_2(ListNode head) {
+        ListNode first = head, secend = head;
+        //先确定链表的中点
+        ListNode h = null, index;
+        while (secend != null && secend.next != null) {
+            index = first;
+            first = first.next;
+            secend = secend.next.next;
+
+            //翻转前面的链表
+            index.next = h;
+            h = index;
+        }
+        //长度为奇数的话，secend不为空，中点需要向前移动一位
+        if (secend != null) {
+            first = first.next;
+        }
+        //开始判断回文
+        index = h;
+        while (first != null && index != null) {
+            if (first.val != index.val) {
+                return false;
+            }
+            first = first.next;
+            index = index.next;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
@@ -47,7 +95,7 @@ public class _234_Solution {
         ListNode node2 = new ListNode(3);
         ListNode node3 = new ListNode(2);
         ListNode node4 = new ListNode(4);
-        ListNode node5 = new ListNode(2);
+        ListNode node5 = new ListNode(3);
         ListNode node6 = new ListNode(3);
         ListNode node7 = new ListNode(1);
         head.next = node2;
@@ -56,7 +104,7 @@ public class _234_Solution {
         node4.next = node5;
         node5.next = node6;
         node6.next = node7;
-        boolean bool = new _234_Solution().isPalindrome(head);
+        boolean bool = new _234_Solution().isPalindrome_2(head);
         System.out.println("_234_Solution.main --> " + bool);
 
     }
---
+++ algorithm/src/main/java/com/leetcode/doublepointer/_345_Solution.java
@@ -0,0 +1,38 @@
+package com.leetcode.doublepointer;
+
+/**
+ * @author brandon
+ * Created on 2019-10-08.
+ * desc: 反转字符串中的元音字母
+ * 编写一个函数，以字符串作为输入，反转该字符串中的元音字母。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: "hello"
+ * 输出: "holle"
+ * 示例 2:
+ * <p>
+ * 输入: "leetcode"
+ * 输出: "leotcede"
+ * 说明:
+ * 元音字母不包含字母"y"。
+ * <p>
+ **/
+public class _345_Solution {
+
+    private char[] chs = {'a', 'e', 'i', 'o', 'u'};
+
+    public String reverseVowels_1(String s) {
+        for (int i = 0; i < s.length(); ++i) {
+
+        }
+
+        return "";
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
